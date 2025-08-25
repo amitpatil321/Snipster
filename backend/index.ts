578bffe5bd69327758c9bf3505b1b9d51eaa6c7a
Amit <amitpatil321@gmail.com>
2025-08-25 19:30:35 +0530

    fix: disable  min request for backend apis

--- backend/index.ts
+++ backend/index.ts
@@ -49,7 +49,7 @@ const config = {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  limit: process.env.NODE_ENV === "production" ? 200 : 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-8",
   legacyHeaders: false,
   ipv6Subnet: 48,
