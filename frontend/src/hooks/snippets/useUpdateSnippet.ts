19c939df372e66e471a5e878b2dd2ec8bfbad85d
Amit <amitpatil321@gmail.com>
2025-09-09 01:10:00 +0530

    fix: update snippet

--- frontend/src/hooks/snippets/useUpdateSnippet.ts
+++ frontend/src/hooks/snippets/useUpdateSnippet.ts
@@ -1,25 +1,33 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { z } from "zod";
 
 import type { snippetSchema } from "@/schema/snippet.schema";
+import type { RootState } from "@/store";
 import type { AxiosError } from "axios";
 
 import { updateSnippet } from "@/services/snippet.service";
+import { setSnippetDetails } from "@/store/app/appSlice";
 
 type SnippetPayload = z.infer<typeof snippetSchema>;
 
 export const useUpdateSnippet = () => {
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const snippetDetails = useSelector(
+    (state: RootState) => state.app.snippetDetails,
+  );
 
   return useMutation({
     mutationFn: (changedFields: SnippetPayload) => updateSnippet(changedFields),
     onSuccess: (response) => {
-      toast.success("Snippet updated successfully!");
       queryClient.invalidateQueries({ queryKey: ["getSnippets", "all"] });
       queryClient.invalidateQueries({
         queryKey: ["getSnippetDetails", response.message.id],
       });
+      if (snippetDetails)
+        dispatch(setSnippetDetails({ ...snippetDetails, ...response.data }));
     },
     onError: (_err) => {
       const err = _err as AxiosError<{ message?: string }>;
