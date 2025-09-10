bd40b0b5a478ec33c17fc2625418b864f11e3954
Amit <amitpatil321@gmail.com>
2025-09-10 13:01:04 +0530

    fix: mongo url

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/snipster";
+const MONGO_URI = process.env.DB_URI || "mongodb://127.0.0.1:27017/snipster";
 
 export const connectDB = async () => {
   try {
